b115e37cb6fd7f455081ff40df00a4d4a077ba5d
sirbobz <bobcertified@gmail.com>
2018-05-18 21:48:28 +0300

    created routes on the nav bar

--- resources/views/Account/core/sidebar.blade.php
+++ resources/views/Account/core/sidebar.blade.php
@@ -9,31 +9,12 @@
         Account
       </li>
       <br>
-      <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Registered Members</a>
-        <ul class="nav-dropdown-items">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('return-view/sms/registered-members') }}"><i class="icon-puzzle"></i> Details</a>
-          </li>
-        </ul>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('return-view/dashboard') }}"><i class="icon-speedometer"></i> Dashboard </a>
       </li>
       <br>
-       <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Sms Records</a>
-        <ul class="nav-dropdown-items">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('return-view/sent-sms') }}"><i class="icon-puzzle"></i> Details</a>
-          </li>
-        </ul>
-      </li>
-      <br>
-      <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-star"></i> Promotion</a>
-        <ul class="nav-dropdown-items">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('return-view/sms/answers-data') }}"><i class="icon-puzzle"></i> Answers</a>
-          </li>
-        </ul>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('return-view/dashboard') }}"><i class="icon-speedometer"></i> Dashboard </a>
       </li>
        <br>
       <li class="divider"></li>
