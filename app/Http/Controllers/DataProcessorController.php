ecb5d486a5c10b003e4508b8f880e6521f86376f
sirbobz <bobcertified@gmail.com>
2018-05-18 22:14:00 +0300

    passed tabular data to view

--- app/Http/Controllers/DataProcessorController.php
+++ app/Http/Controllers/DataProcessorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Dataset;
+use Cache, Exception;
 
 class DataProcessorController extends Controller
 {
@@ -18,14 +19,25 @@ class DataProcessorController extends Controller
     }
 
     /**
-     * Show allData from database.
-     * Data Cached
+     * Show allData from database 
+     * in tabular manner.
      *
      * @return \Illuminate\Http\Response
      */
     public function allData()
     {
-        return view('Account.dashboard');
+    	try 
+    	{
+            $allData = Dataset::all();
+    		
+    	    return view('Account.DataAnalysis.tabular_records', compact('allData'));
+
+    	} 
+    	catch (Exception $e) 
+    	{
+    		
+    	}
+     
     }
     
 }
