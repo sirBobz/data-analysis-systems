10797493d12352858dbf838ff0d688f4bc49cc6f
sirbobz <bobcertified@gmail.com>
2018-05-19 08:49:41 +0300

    updated the app version

--- resources/views/Account/core/footer.blade.php
+++ resources/views/Account/core/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="app-footer">
     <div class="text-center">
-        &copy;  Statum Company Limited {{ date("Y") }}  &emsp; &emsp; {{ config('app.version', 'Version 1.0.0') }}
+        &copy;  {{ config('app.name', 'Laravel') }} {{ date("Y") }}  &emsp; &emsp; {{ config('app.version', 'Version 1.0.0') }}
    </div>
 </footer>
